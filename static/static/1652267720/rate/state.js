9f078d72085bd85be6c874a6a4803e2d05e75bc1
Alexander Rybakov <rybakov_web@outlook.com>
2022-05-11 14:15:42 +0300

    new land

---
+++ static/static/1652267720/rate/state.js
@@ -0,0 +1,1 @@
+window.__APP__=(function(a){return {staticAssetsBase:"\u002Fparibet\u002Fstatic\u002Fstatic\u002F1652267720",layout:"default",error:a,state:{app:{},voting:{items:a},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Frate",config:{_app:{basePath:"\u002Fparibet\u002F",assetsPath:"\u002Fparibet\u002Fstatic\u002F",cdnURL:a}}}}(null));
